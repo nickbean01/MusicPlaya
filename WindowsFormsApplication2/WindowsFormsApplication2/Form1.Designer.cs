9972b6cc2df18a28c98d980503dd9f53ca494579
Nick Bené <bene.nick1@gmail.com>
2015-07-27 19:32:29 -0400

    First Commit
    gr8 job

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ScanRoot = new System.Windows.Forms.Button();
+            this.StatusBox = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ArtistListBox = new System.Windows.Forms.ListBox();
+            this.AlbumListBox = new System.Windows.Forms.ListBox();
+            this.TrackListBox = new System.Windows.Forms.ListBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList3 = new System.Windows.Forms.ImageList(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.ScanRoot);
+            this.panel1.Controls.Add(this.StatusBox);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1017, 84);
+            this.panel1.TabIndex = 1;
+            // 
+            // ScanRoot
+            // 
+            this.ScanRoot.Location = new System.Drawing.Point(939, 4);
+            this.ScanRoot.Name = "ScanRoot";
+            this.ScanRoot.Size = new System.Drawing.Size(75, 23);
+            this.ScanRoot.TabIndex = 4;
+            this.ScanRoot.Text = "Scan Root";
+            this.ScanRoot.UseVisualStyleBackColor = true;
+            this.ScanRoot.Click += new System.EventHandler(this.ScanRoot_Click);
+            // 
+            // StatusBox
+            // 
+            this.StatusBox.Location = new System.Drawing.Point(394, 47);
+            this.StatusBox.Name = "StatusBox";
+            this.StatusBox.Size = new System.Drawing.Size(237, 20);
+            this.StatusBox.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(556, 18);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(475, 18);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(394, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // ArtistListBox
+            // 
+            this.ArtistListBox.FormattingEnabled = true;
+            this.ArtistListBox.Location = new System.Drawing.Point(12, 102);
+            this.ArtistListBox.Name = "ArtistListBox";
+            this.ArtistListBox.Size = new System.Drawing.Size(183, 459);
+            this.ArtistListBox.TabIndex = 2;
+            // 
+            // AlbumListBox
+            // 
+            this.AlbumListBox.FormattingEnabled = true;
+            this.AlbumListBox.Location = new System.Drawing.Point(201, 102);
+            this.AlbumListBox.Name = "AlbumListBox";
+            this.AlbumListBox.Size = new System.Drawing.Size(206, 459);
+            this.AlbumListBox.TabIndex = 3;
+            // 
+            // TrackListBox
+            // 
+            this.TrackListBox.FormattingEnabled = true;
+            this.TrackListBox.Location = new System.Drawing.Point(413, 102);
+            this.TrackListBox.Name = "TrackListBox";
+            this.TrackListBox.Size = new System.Drawing.Size(616, 459);
+            this.TrackListBox.TabIndex = 4;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1042, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // imageList2
+            // 
+            this.imageList2.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList2.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // imageList3
+            // 
+            this.imageList3.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList3.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList3.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1042, 572);
+            this.Controls.Add(this.TrackListBox);
+            this.Controls.Add(this.AlbumListBox);
+            this.Controls.Add(this.ArtistListBox);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox ArtistListBox;
+        private System.Windows.Forms.ListBox AlbumListBox;
+        private System.Windows.Forms.ListBox TrackListBox;
+        private System.Windows.Forms.TextBox StatusBox;
+        private System.Windows.Forms.Button ScanRoot;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ImageList imageList2;
+        private System.Windows.Forms.ImageList imageList3;
+    }
+}
+
---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        string RootDirectory = "E:\\Music\\TEST FOLDER";
+        string FileType = ".mp3";
+        string xmlPath = "E:\\Music\\TestLib.xml";
+        string newFile = "E:\\Music\\test3.xml";
+
+        public Form1()
+        {
+            InitializeComponent();
+            ReadLibrary();
+        }
+
+        /* reads from XML library file */
+        public void ReadLibrary()
+        {
+            XDocument doc;
+            if (!File.Exists(xmlPath))
+            {
+                CreateLibrary();
+            }
+            else
+            {
+                
+            }
+        }
+
+        /* add all artists to artist list */
+        public void PopulateArtists()
+        {
+            XDocument doc = XDocument.Load(xmlPath);
+            ArtistListBox.Items.Add("All Artists");
+            /*foreach (var name in doc.Root.Element("Artist").OfType<XElement>()
+                .Select(x => x.Name).Distinct())
+            {
+                System.Diagnostics.Debug.WriteLine(name);
+            }*/
+        }
+        
+        /* add albums depending on artist selected */
+        public void PopulateAlbums()
+        {
+            AlbumListBox.Items.Add("All Albums");
+        }
+
+        /* add songs depending on album(s) selected */
+        public void PopulateTracks()
+        {
+            
+        }
+
+        /* creates XML library file */
+        public void CreateLibrary()
+        {
+            if (File.Exists(xmlPath))
+            {
+                // you goofed, should never happen
+                return;
+            }
+            ScanDirectory(RootDirectory);
+        }
+
+        /* scans given root directory and adds missing files to XML library */
+        public void ScanDirectory(String TargetDirectory)
+        {
+            string[] FileEntries;
+            
+
+            if (!Directory.Exists(TargetDirectory))
+            {
+                System.Diagnostics.Debug.WriteLine("\"" + TargetDirectory + "\"  does not exist.");
+                return;
+            }
+            System.Diagnostics.Debug.WriteLine("Scanning: " + TargetDirectory);
+
+            FileEntries = Directory.GetFileSystemEntries(TargetDirectory);
+            foreach (string TargetPath in FileEntries)
+            {
+                if (File.Exists(TargetPath))            // make sure file exists
+                {
+                    if (TargetPath.EndsWith(FileType))  // check if mp3
+                    {
+                        TrackListBox.Items.Add(TargetPath);
+
+                        // first check if the path already exists in the XML file                      
+                        InsertTrack(TargetPath);                        
+                    }
+                }
+                else if (Directory.Exists(TargetPath))
+                {
+                    ScanDirectory(TargetPath);
+                }
+                else
+                {
+                    System.Diagnostics.Debug.WriteLine(TargetPath + " does not exist or cannot be opened.");
+                }
+            }         
+            return;
+        }
+
+        /* inserts new track to XML file */
+        /* pulls info from metadata (eventually) */
+        private void InsertTrack(string TargetPath)
+        {
+            string title;
+            string album;
+            string artist;
+            int trackNum;
+            int num;
+            int year;
+            string dateAdded;
+
+            // dumby info for testing
+            title = "Hysteria";
+            album = "Zoo";
+            artist = "Ceremony";
+            trackNum = 1;
+            num = 1;
+            year = 2012;
+            dateAdded = "7-27-2015";
+
+            XElement Track =
+                new XElement("Track",
+                    new XAttribute("ID", num),
+                    new XElement("Title", title),
+                    new XElement("Album", album),
+                    new XElement("Artist", artist),
+                    new XElement("TrackNumber", trackNum),
+                    new XElement("Year", year),
+                    new XElement("DateAdded", dateAdded),
+                    new XElement("Path", TargetPath)
+                );
+            // need to increment 'global' counter in XML to prevent duplicate Track ID attribute
+            // maybe just use path as a 'key'? would prevent duplicates no probbbb
+            System.Diagnostics.Debug.WriteLine(Track);
+        }
+
+        private void ScanRoot_Click(object sender, EventArgs e)
+        {
+            ScanDirectory(RootDirectory);
+        }
+    }
+}
